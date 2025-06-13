40798941fbc3efe572fcb59c12e7bb6c8e514fe0
moebadarneh <115925072+moebadarneh@users.noreply.github.com>
2025-06-13 19:55:16 +0000

    Add taxi zones asset to load dataset into DuckDB

--- dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
+++ dagster_university/dagster_essentials/dagster_essentials/assets/trips.py
@@ -65,3 +65,34 @@ def taxi_trips() -> None:
         max_retries=10,
     )
     conn.execute(query)
+
+
+
+@dg.asset(
+    deps=["taxi_zones_file"]
+)
+def taxi_zones() -> None:
+    """
+      The raw taxi zones dataset, loaded into a DuckDB database
+    """
+    query = f"""
+        create or replace table zones as (
+          select
+            LocationID  as zone_id,
+            zone,
+            borough,
+            the_geom  as geometry
+            
+          from '{constants.TAXI_ZONES_FILE_PATH}'
+        );
+    """
+
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={
+            "database": os.getenv("DUCKDB_DATABASE"),
+        },
+        max_retries=10,
+    )
+    conn.execute(query)
